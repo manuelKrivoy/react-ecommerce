db9c3ef1e7d46aa1ee840aec90e2866eae0dad6a
Manuel Krivoy <krivoymanuel@gmail.com>
2024-02-04 12:43:24 -0300

    estilosNavbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,15 +3,15 @@ import { CartWidget } from "../common/CartWidget";
 
 
 export const Navbar = () => {
- return (
-  <AppBar position="static" sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px' }}>
-    <div>
-    <Button variant="contained" style={{ marginRight: '8px', marginBottom:'5px' }}>Home</Button>
-        <Button variant="contained" style={{ marginRight: '8px', marginBottom:'5px'}}>Camisetas</Button>
-        <Button variant="contained" style={{ marginRight: '8px', marginBottom:'5px'}} >Accesorios</Button> 
-    </div>
+  return (
+    <AppBar position="static" sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '5px' }}>
       <div>
-      <CartWidget/>
+        <Button variant="contained" sx={{marginRight: '8px', marginBottom:'5px'}}>Home</Button>
+        <Button variant="contained" sx={{marginRight: '8px', marginBottom:'5px'}}>Camisetas</Button>
+        <Button variant="contained" sx={{marginRight: '8px', marginBottom:'5px'}}>Accesorios</Button>
+      </div>
+      <div>
+        <CartWidget/>
       </div>
     </AppBar>
   );
