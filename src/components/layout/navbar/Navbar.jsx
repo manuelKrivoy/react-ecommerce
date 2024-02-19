225dc49b18fbeecccebe675413adedda8c56da4d
Manuel Krivoy <krivoymanuel@gmail.com>
2024-02-19 14:14:19 -0300

    cargo fuente de forma local

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,16 +1,9 @@
-import { Helmet } from "react-helmet";
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
 import { CartWidget } from "../../common/cartWidget/CartWidget";
 
 export const Navbar = () => {
   return (
     <>
-      <Helmet>
-        {/* Utilizamos Helmet para agregar etiquetas <link> necesarias para Google Fonts */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Protest+Riot&display=swap" rel="stylesheet" />
-      </Helmet>
       <Flex justifyContent="space-between" alignItems="center" p="5" background="#233239">
         <Box>
           <Button variant="solid" mr="4" background="#3FBD41">
--- src/theme.jsx
+++ src/theme.jsx
@@ -1,9 +1,15 @@
 import { extendTheme } from "@chakra-ui/react";
-
+import "./fonts.css"; //Importo fuentes para poder utilizarlas en cualquier parte del proyecto
 //Este archivo reemplaza al index.css. Chakra UI no me dejaba setear un background global con body
 //Esta librería provee esta posibilidad de crear aspectos globales
 
 const theme = extendTheme({
+  //Este bloque define fuentes globales para el proyecto.
+  // fonts: {
+  //   body: "'Protest Riot', sans-serif",
+  //   heading: " sans-serif",
+  // },
+
   styles: {
     global: {
       body: {
