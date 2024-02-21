136287c4154f1d7213213d956f5e2aa71c4e30ab
Manuel Krivoy <krivoymanuel@gmail.com>
2024-02-21 20:48:56 -0300

    homebutton

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,25 +1,26 @@
-import { Flex, Box, Text, Link, Button } from "@chakra-ui/react";
+import { Flex, Box, Text, Link, Button, Image, Img } from "@chakra-ui/react";
 import NavButton from "../../common/NavButton";
 import { CartWidget } from "../../common/cartWidget/CartWidget";
 
 export const Navbar = () => {
   return (
     <Flex justifyContent="space-between" alignItems="center" p="5" background="#233239">
-      <Box>
-        <Link href="/">
-          <Button variant="solid" mr="4" background="#3FBD41">
-            <img src="/logo.png" alt="Logo" style={{ width: "30px", height: "30px" }} />
-          </Button>
-        </Link>
+      <Box display="flex" alignItems="center">
+        <NavButton text={<Image src="/logo.png" alt="Logo" boxSize="40px" />} href="/" />
         <NavButton text="Selecciones" href="/category/Selecciones" />
         <NavButton text="Clubes" href="/category/Clubes" />
         <NavButton text="Accesorios" href="/category/Accesorios" />
       </Box>
-      <Box>
-        <Text fontSize="3xl" color="#D8731F" as="em" fontFamily="'Protest Riot', sans-serif">
-          MANSIÓN DE FÚTBOL
-        </Text>
-      </Box>
+      <Flex alignItems="center">
+        <Box>
+          <Text fontSize="3xl" color="#D8731F" as="em" fontFamily="'Protest Riot', sans-serif">
+            MANSIÓN DE FÚTBOL
+          </Text>
+        </Box>
+        <Box ml="15px">
+          <Image src="/logo.png" alt="Logo" boxSize="50px" />
+        </Box>
+      </Flex>
       <Box>
         <Link href="/cart">
           <CartWidget />
