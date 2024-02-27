04849237d009e79fb029de9c024ea1ec2e5ff407
Manuel Krivoy <krivoymanuel@gmail.com>
2024-02-27 11:32:06 -0300

    404 page

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,5 +1,17 @@
+import { Center, Box, Image, Text } from "@chakra-ui/react";
+import { NavButton } from "../common/NavButton";
+
 export const NotFound = () => {
-  return <div> 404 NotFound</div>;
+  return (
+    <Center>
+      <Box boxSize="lg">
+        <Image src="/404.gif" maxH="100%" maxW="100%" />
+        <Box position="absolute" top="100%" left="50%" transform="translate(-50%, -50%)">
+          <NavButton mt="10px" text="Volver al inicio" href="/" />
+        </Box>
+      </Box>
+    </Center>
+  );
 };
 
 export default NotFound;
