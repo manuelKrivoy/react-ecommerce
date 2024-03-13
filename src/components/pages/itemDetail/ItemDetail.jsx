0dca72ca926016f0547ddec7e39067c925138ec6
Manuel Krivoy <krivoymanuel@gmail.com>
2024-03-13 12:26:52 -0300

    final

--- src/components/pages/itemDetail/ItemDetail.jsx
+++ src/components/pages/itemDetail/ItemDetail.jsx
@@ -9,28 +9,26 @@ const ItemDetail = ({ item, onAdd, totalQuantity }) => {
 
   return (
     <Flex
-      direction={{ base: "column", md: "row" }} // Cambia la dirección de columna a fila en dispositivos móviles
+      direction={{ base: "column", md: "row" }}
       alignItems="flex-start"
       justifyContent="flex-start"
       p="20px"
-      pl={{ base: "20px", md: "40px" }} // Ajusta el padding izquierdo en dispositivos móviles
+      pl={{ base: "20px", md: "40px" }}
     >
       <Image
         src={item.img}
         alt={item.title}
-        boxSize={{ base: "300px", md: "400px" }} // Ajusta el tamaño de la imagen para dispositivos móviles
+        boxSize={{ base: "300px", md: "400px" }}
         objectFit="contain"
         border="1px solid black"
-        mr={{ base: "0", md: "40px" }} // Ajusta el margen derecho en dispositivos móviles
-        mb={{ base: "20px", md: "0" }} // Ajusta el margen inferior en dispositivos móviles
+        mr={{ base: "0", md: "40px" }}
+        mb={{ base: "20px", md: "0" }}
         borderRadius="10%"
         _hover={{ opacity: "70%", transform: "scale(1.009)", cursor: "default" }}
         bg="white"
       />
       <Box>
         <Heading as="h2" fontSize={{ base: "35px", md: "55px" }} mb="20px">
-          {" "}
-          {/* Ajusta el tamaño del título para dispositivos móviles */}
           {item.title}
         </Heading>
         <Text mb="20px" fontSize="20px">
